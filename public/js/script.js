5f78ff1ba8d076f126bc730ad21ac9249f75d192
Anjali-Raj07 <anjalirajt@gmail.com>
2024-09-04 18:31:12 +0530

    UI Updation

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,23 @@
-// const updateUser = async (userId, updates) => {
-//     const updatedUser = await data.findByIdAndUpdate(
-//         userId,
-//         { ...updates, updatedTime: new Date() },  
-//         { new: true }
-//     );
-//     return updatedUser;
-// };
+
+// socket.io
 const socket = io();
-      socket.on("user details",(userdetails)=>{
-           console.log(userdetails,"user");
-           
-      })
-      const user = {
-                id: "<%= user.id %>",
-                firstName: "<%= user.FirstName %>",
-                lastName: "<%= user.LastName %>",
-                email: "<%= user.Email %>",
-                role: "<%= user.Role %>"
-            };
-            if(user){
-                socket.emit("login details",user)
-            }
-            socket.emit("getuser details")
+socket.on("user details", (userdetails) => {
+    console.log(userdetails, "user");
+
+})
+const user = {
+    id: "<%= user.id %>",
+    firstName: "<%= user.FirstName %>",
+    lastName: "<%= user.LastName %>",
+    email: "<%= user.Email %>",
+    role: "<%= user.Role %>"
+};
+if (user) {
+    socket.emit("login details", user)
+}
+socket.emit("getuser details")
+
+
+
+
+
