7a871fc45f3da1cac8bb9edf19d36d3286bf076a
Anjali-Raj07 <164318600+Anjali-Raj07@users.noreply.github.com>
2024-09-04 18:09:16 +0530

    Update updatedDataModel.js

--- models/updatedDataModel.js
+++ models/updatedDataModel.js
@@ -38,11 +38,3 @@ const UpdatedData = mongoose.model('updatedData', UpdatedDataSchema);
 
 module.exports = UpdatedData;
 
-// #User Activity logs
-// 1. Save Last Login Details of the user using help of socketio package
-// 2. Save in a different collection of the updated fields with user details when
-// changed
-// Example:
-// 3 Feb 2021 is changed to 4 Feb 2021
-// 3 Feb 2021 was updated by the user xxx to 4th Feb 2021 should be the response and save it
-// in the database with FieldOldvalue,FieldNewvalue,Userid,Username
